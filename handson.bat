958d12908151ae980eb8113f2edfbcf378619380
Luc Tremsal <luc.tremsal@datalyo.com>
2020-01-07 17:27:14 +0100

    Creation de init.bat pour ajouter Sublime Text au PATH. Modification de handson.bat pour ouvrir le code dans Sublime Text directement. Ajout de commentaires pour mieux guider l'utilisateur

--- handson.bat
+++ handson.bat
@@ -1,18 +1,53 @@
 @REM SBT launcher script
 @REM 
-@REM Envioronment:
+@REM Environment:
 @REM JAVA_HOME - location of a JDK home dir (mandatory)
 @REM SBT_OPTS  - JVM options (optional)
 
-
 @setlocal
+
+set SBT_HOME=%~dp0
+
+@echo off
+set /p REPONSE="Premiere fois(P)/Reprendre(R): "
+
+if %REPONSE% == P (
+    start sublime.exe "%SBT_HOME%\src\test\scala\partie1-1\e0_vars_vals.scala"
+    echo Sublime Text s'ouvre avec le dernier fichier edite. Ouvrez les prochains exercices lorsque vous avancez.
+    echo               --------
+    echo Vous pouvez fermer l'autre invite de commandes.
+    echo               --------
+    echo Executer go pour lancer le projet. Lorsque vous avez effectue vos changements de code sur Sublime Text, enregistrez les et verifiez les resultas ci-dessous.
+    echo               --------
+    echo Rappel :
+    echo TEST FAILED : au moins un des tests de l'exercice en cours est faux. Des indications sont affichees plus bas.
+    echo TEST PENDING : Vous n'avez pas modifie les '_' de l'exercice OU les tests de l'exercice en cours sont tous justes et vous pouvez passer a la suite.
+    echo               --------
+    echo Le premier exercice devrait s'ouvrir dans Sublime Text.
+    echo               --------
+) else (
+	echo cas 2
+    start sublime.exe
+    echo Sublime Text s'ouvre avec le dernier fichier edite. Ouvrez les prochains exercices lorsque vous avancez.
+    echo               --------
+    echo Vous pouvez fermer l'autre invite de commandes.
+    echo               --------
+    echo Executer go pour lancer le projet. Lorsque vous avez effectue vos changements de code sur Sublime Text, enregistrez les et verifiez les resultas ci-dessous.
+    echo               --------
+    echo Rappel :
+    echo TEST FAILED : au moins un des tests de l'exercice en cours est faux. Des indications sont affichees plus bas.
+    echo TEST PENDING : Vous n'avez pas modifie les '_' de l'exercice OU les tests de l'exercice en cours sont tous justes et vous pouvez passer a la suite.
+    echo               --------
+)
 
 @echo off
 
-chcp 65001
+REM Check if Sublime Text is already in PATH and add it if not
 
 set SBT_HOME=%~dp0
 set ERROR_CODE=0
+
+chcp 65001
 
 rem We use the value of the JAVACMD environment variable if defined
 set _JAVACMD=%JAVACMD%
@@ -45,10 +80,6 @@ set ERROR_CODE=1
 @endlocal
 
 exit /B %ERROR_CODE%
-
-
-
-
 
 rem set SCRIPT_DIR=%~dp0
 
---
+++ init.bat
@@ -0,0 +1,41 @@
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------    
+
+where /q sublime_text
+IF ERRORLEVEL 1 (
+    echo Ajout de Sublime Text dans PATH.
+    setx path "%PATH%C:\Program Files\Sublime Text 3;" /M
+    echo               --------
+) ELSE (
+	echo Sublime Text deja dans le PATH
+)
+
+start cmd /c handson.bat
+
+exit
